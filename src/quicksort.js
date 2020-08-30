4c891f0a52ee2f1a0dba7a0594863daa52d13fc5
Tony Le <tle@kabbage.com>
2020-08-29 20:55:14 -0400

    quicksort implementation

---
+++ src/quicksort.js
@@ -0,0 +1,77 @@
+const testArrays = [
+  [43, 3, 545, 34, 53, 31, 657, 445, 56, 23],
+  [1, 1, 1, 1, 2, 2, 2, 2, 2, 5, 756, 756, 756, 756],
+  [1, 56564, 45645, 56564, 56564, 56564, 2],
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 3],
+  [
+    453345,
+    345,
+    435346,
+    3216743,
+    655,
+    435634,
+    34,
+    2435,
+    623,
+    46,
+    5,
+    545326,
+    2346
+  ]
+]
+
+const swap = (array, indexA, indexB) => {
+  const a = array[indexA]
+  array[indexA] = array[indexB]
+  array[indexB] = a
+}
+
+const partition = (array, lo, hi) => {
+  let pivot = Math.floor(Math.random() * (hi - lo + 1)) + lo
+  swap(array, lo, pivot)
+  pivot = lo
+
+  let left = lo + 1
+  let right = hi
+  while (left <= right) {
+    if (array[left] <= array[pivot]) {
+      left++
+    } else {
+      swap(array, left, right)
+      right--
+    }
+  }
+
+  swap(array, pivot, left - 1)
+
+  return left - 1
+}
+
+const quicksort = (array, lo = 0, hi = array.length - 1) => {
+  if (hi <= lo) {
+    return
+  }
+
+  const mid = partition(array, lo, hi)
+
+  quicksort(array, lo, mid - 1)
+  quicksort(array, mid + 1, hi)
+
+  return
+}
+
+const isSorted = (array) => {
+  for (let index = 0; index < array.length - 1; index++) {
+    if (array[index] > array[index + 1]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+testArrays.map((array) => {
+  quicksort(array)
+  console.log(array)
+  console.log(isSorted(array))
+})
